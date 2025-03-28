36e7caac6f7f403c210ff2f1b1e4349bcb516e0d
Arihant Choudhary <arihant@berkeley.edu>
2025-03-27 22:48:56 -0700

    Fix Vercel build error by inlining cn utility function in tooltip component

--- klaritydocs/src/components/ui/tooltip.tsx
+++ klaritydocs/src/components/ui/tooltip.tsx
@@ -1,7 +1,12 @@
 import * as React from "react"
 import * as TooltipPrimitive from "@radix-ui/react-tooltip"
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-import { cn } from "@/lib/utils"
+// Include the utility function directly to avoid import issues
+function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
 
 const TooltipProvider = TooltipPrimitive.Provider
 
